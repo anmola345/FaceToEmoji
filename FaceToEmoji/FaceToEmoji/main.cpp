c39e474ea0254a044aa206efe13a7d2203e0b3dd
anmolagrawal345 <anmolagrawal345@gmail.com>
2018-11-11 23:24:17 -0600

    setting up project

---
+++ FaceToEmoji/FaceToEmoji/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  FaceToEmoji
+//
+//  Created by Anmol Agrawal on 11/11/18.
+//  Copyright © 2018 Anmol Agrawal. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
