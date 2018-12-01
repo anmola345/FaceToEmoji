8100e4568315cf433487381a76e2c8fb83bda9a3
anmolagrawal345 <anmolagrawal345@gmail.com>
2018-12-01 16:24:15 -0600

    adding picture taking

--- EmotionDetectionOpenCV/EmotionDetectionOpenCV/main.cpp
+++ EmotionDetectionOpenCV/EmotionDetectionOpenCV/main.cpp
@@ -1,12 +1,13 @@
 //
 //  main.cpp
-//  FaceToEmoji
+//  EmotionDetectionOpenCV
 //
-//  Created by Anmol Agrawal on 11/11/18.
+//  Created by Anmol Agrawal on 11/12/18.
 //  Copyright © 2018 Anmol Agrawal. All rights reserved.
 //
 
 #include <iostream>
+//#include <opencv2/opencv.hpp>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
---
+++ FaceToEmoji/FaceToEmoji/FaceToEmoji.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  FaceToEmoji
+//
+//  Created by Anmol Agrawal on 11/11/18.
+//  Copyright © 2018 Anmol Agrawal. All rights reserved.
+//
+
+/*
+ * File:   main.cpp
+ * Author: sagar
+ *
+ * Created on 10 September, 2012, 7:48 PM
+ */
+
+
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+
+int main() {
+    
+    cout << "Hello World" << endl;
+}
